5bf2efc6a07ef4186e61155e3f034049f3dc60e8
IBJD <ibjanardana@gmail.com>
2023-01-16 11:57:10 +0700

    update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,17 +2,15 @@ import User from "../models/UserModels.js";
 import { Op } from "sequelize";
 
 export const getUsers = async(req, res) => {
-    const last_id = parseInt(req.query.lastId) || 0;
-    const limit = parseInt(req.query.limit) || 10;
-    const search = req.query.search_query || "";
-    
+    const last_id =parseInt(req.query.lastId) || 0; 
+    const limit =parseInt(req.query.limit) || 10; 
+    const search = req.query.search_query || ""; 
+
     let result = [];
-
     if(last_id < 1){
-
         const results = await User.findAll({
             where:{
-                [Op.or]:[{name:{
+                [Op.or]: [{name:{
                     [Op.like]: '%'+search+'%'
                 }}, {email:{
                     [Op.like]: '%'+search+'%'
@@ -20,34 +18,36 @@ export const getUsers = async(req, res) => {
             },
             limit: limit,
             order:[
-                ['id','DESC']
+                ['id', 'DESC']
             ]
         });
-        result = results;
-    }else{
 
+        result = results;
+
+    }else{
         const results = await User.findAll({
             where:{
                 id:{
                     [Op.lt]: last_id
                 },
-                [Op.or]:[{name:{
+                [Op.or]: [{name:{
                     [Op.like]: '%'+search+'%'
-                }}, {email:{
+                }},{email:{
                     [Op.like]: '%'+search+'%'
                 }}]
             },
             limit: limit,
             order:[
-                ['id','DESC']
+                ['id', 'DESC']
             ]
         });
+
         result = results;
     }
     res.json({
         result: result,
         lastId: result.length ? result[result.length -1].id : 0,
         hasMore: result.length >= limit ? true : false
-    })
-    
+});
+
 }
